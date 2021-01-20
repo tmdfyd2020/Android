04602d228ffefa0f8c12c3c643ea5132b3d55d20
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-01-21 01:53:37 +0900

    2021-01-16

--- 009_Change Image View.java
+++ 009_Change Image View.java
@@ -19,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.imageView);
         imageView2 = findViewById(R.id.imageView2);
-        // ID를 사용하여 뷰를 찾는 메소드, id가 인자로 들어간다.
+        // □ findViewById : XML 파일에서 추가한 뷰의 ID를 사용하여 뷰를 찾는 메소드, id가 인자로 들어간다.
     }
-    // XML 레이아웃 파일에서 id 지정할 때 -> @+id/아이디
-    // 자바 소스 코드에서 참조할 때 -> R.id.아이디
+    // □ XML 레이아웃 파일에서 id 지정할 때 -> @+id/아이디
+    // □ 자바 소스 코드에서 참조할 때 -> R.id.아이디
 
     public void onButton1Clicked(View v) {
         changeImage();
@@ -44,10 +44,8 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-// 하나의 화면은 XML 파일과 소스 파일로 나누어져 있다. 이 두 개의 파일이 쌍을 이루면서 하나의 화면을 만든다.
+// □ 하나의 화면은 XML 파일과 소스 파일로 나누어져 있다. 이 두 개의 파일이 쌍을 이루면서 하나의 화면을 만든다.
 
-// 소스 파일에 있는 setContentView 메소드가 파라미터로 XML 파일을 넘겨줄 수 있는데, 이 파라미터를 이용해
-// 소스 파일과 XML 파일이 연결된다.
+// □ 소스 파일에 있는 setContentView 메소드가 파라미터로 XML 파일을 넘겨줄 수 있는데, 이 파라미터를 이용해 소스 파일과 XML 파일이 연결된다.
 
-// 화면에서 이미지가 바뀌어 보이는 것처럼 보이지만 실제로는 중첩된 이미지 상태에서 하나를 보이게 하거나
-// 하나를 보이지 않게 하는 방법을 사용한 것이다.
+// □ 화면에서 이미지가 바뀌어 보이는 것처럼 보이지만 실제로는 중첩된 이미지 상태에서 하나를 보이게 하거나 하나를 보이지 않게 하는 방법을 사용한 것이다.
