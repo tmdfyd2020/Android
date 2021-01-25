f9f22d205c8643f316eb6077518c078c7cf76806
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-01-26 01:35:44 +0900

    2021-01-26

---
+++ Utube Lecture/01_Calculator/Calculator.java
@@ -0,0 +1,57 @@
+package org.techtown.lecture_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addClick(View v) {
+        // xml 파일에서 id를 받아와서 객체를 생성한다.
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1 + n2));
+    }
+
+    public void subtractClick(View v) {
+        // xml 파일에서 id를 받아와서 객체를 생성한다.
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1 - n2));
+    }
+
+    public void multiplyClick(View v) {
+        // xml 파일에서 id를 받아와서 객체를 생성한다.
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1 * n2));
+    }
+
+    public void divideClick(View v) {
+        // xml 파일에서 id를 받아와서 객체를 생성한다.
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1 / n2));
+    }
+}
