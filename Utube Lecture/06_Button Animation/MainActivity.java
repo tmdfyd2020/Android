44e8190a283003d9266afb51ec92a6167394f3b8
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-01-28 01:30:50 +0900

    2021-01-28

---
+++ Utube Lecture/06_Button Animation/MainActivity.java
@@ -0,0 +1,17 @@
+// 이미지 파일 3개를 drawable 폴더에 저장
+// button1.xml 파일을 drawable 폴더에 생성
+
+package org.techtown.tutorial8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
