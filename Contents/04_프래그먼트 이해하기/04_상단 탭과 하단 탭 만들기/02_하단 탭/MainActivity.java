a647ecc889855f62c5249a321fd1eec516fd13f0
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-02-10 17:25:42 +0900

    하단 탭 보여주기

---
+++ Contents/04_프래그먼트 이해하기/04_상단 탭과 하단 탭 만들기/02_하단 탭/MainActivity.java
@@ -0,0 +1,47 @@
+package org.techtown.tab2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);  // 하단 네비게이션 바 불러오기
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {  // 네비게이션 바 클릭시
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.tab1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();  // container에 fragment1 불러오기
+                        return true;
+                    case R.id.tab2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();  // container에 fragment2 불러오기
+                        return true;
+                    case R.id.tab3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();  // container에 fragment3 불러오기
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
