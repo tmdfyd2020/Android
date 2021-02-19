ac160ca2a85bb49e01fc34d9709acfbdd8486444
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-02-19 17:29:47 +0900

    앱 화면에 웹브라우저 넣기

---
+++ Contents/07_애니메이션과 다양한 위젯 사용하기/03_앱 화면에 웹브라우저 넣기/MainActivity.java
@@ -0,0 +1,62 @@
+package org.techtown.web;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        webView = findViewById(R.id.webView);
+
+        /* 웹뷰의 설정 수정하기 */
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);  // 자바스크립트가 동작할 수 있는 환경이 된다.
+
+        //webView.setWebChromeClient(new BrowserClient());
+        webView.setWebViewClient(new ViewClient());
+
+        //webView.addJavascriptInterface(new JavaScriptMethods(), "sample");
+        //webView.loadUrl("file:///android_asset/www/sample.html");
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                webView.loadUrl(editText.getText().toString());  // 버튼 클릭 시 사이트 로딩하기
+            }
+        });
+    }
+
+    final class BrowserClient extends WebChromeClient {
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            result.confirm();
+
+            return true;
+        }
+    }
+
+    private class ViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
+            view.loadUrl(url);
+
+            return true;
+        }
+    }
+
+}
