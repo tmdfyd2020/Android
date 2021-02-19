838ad7f4606ff53445f8e16d7f700381ebb8f14d
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-02-20 02:05:08 +0900

    스레드로 애니메이션 만들기

---
+++ Contents/08_스레드와 핸들러 이해하기/05_스레드로 애니메이션 만들기/MainActivity.java
@@ -0,0 +1,71 @@
+package org.techtown.threadanim;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    ArrayList<Drawable> drawableList = new ArrayList<Drawable>();
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        drawableList.add(res.getDrawable(R.drawable.face1));
+        drawableList.add(res.getDrawable(R.drawable.face2));
+        drawableList.add(res.getDrawable(R.drawable.face3));
+        drawableList.add(res.getDrawable(R.drawable.face4));
+        drawableList.add(res.getDrawable(R.drawable.face5));
+
+        imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* 애니메이션을 위한 스레드 객체 만들어 실행하기 */
+                AnimThread thread = new AnimThread();
+                thread.start();
+            }
+        });
+    }
+
+    class AnimThread extends Thread {
+        public void run() {
+            int index = 0;
+            for (int i = 0; i < 100; i++) {
+                final Drawable drawable = drawableList.get(index);
+                index += 1;
+                if (index > 4) {
+                    index = 0;
+                }
+
+                /* 화면에 이미지를 보여주기 위해 핸들러의 post() 메서드 호출하기 */
+                handler.post(new Runnable() {
+                    public void run() {
+                        imageView.setImageDrawable(drawable);
+                    }
+                });
+
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
