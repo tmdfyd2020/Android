fa55dc8e142608933dac2c88fd2ddfba6321b537
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-02-21 01:29:31 +0900

    JSON 데이터 다루기

---
+++ Contents/09_서버에 데이터 요청하고 응답받기/05_JSON 데이터 다루기/Movie.java
@@ -0,0 +1,21 @@
+package org.techtown.request;
+
+public class Movie {
+
+    String movieCd;
+    String movieNm;
+    String movieNmEn;
+    String prdtYear;
+    String openDt;
+    String typeNm;
+    String prdtStatNm;
+    String nationAlt;
+    String genreAlt;
+    String repNationNm;
+    String repGenrmNm;
+    String directors;
+    String companys;
+    String companyCd;
+    String companyNm;
+}
+
