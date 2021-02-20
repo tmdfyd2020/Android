3da4b79747e2a8073046ef59a120a4724bb1537b
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-02-21 01:30:20 +0900

    JSON 데이터 다루기

---
+++ Contents/09_서버에 데이터 요청하고 응답받기/05_JSON 데이터 다루기/MovieListResult.java
@@ -0,0 +1,14 @@
+package org.techtown.request;
+
+import android.graphics.Movie;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String totCnt;
+    String source;
+
+    ArrayList<Movie> movieList = new ArrayList<Movie>();
+
+}
