b5518587e8ea9dbf34e7757164f587c46fa8c4f4
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-02-21 01:29:58 +0900

    JSON 데이터 다루기

---
+++ Contents/09_서버에 데이터 요청하고 응답받기/05_JSON 데이터 다루기/MovieList.java
@@ -0,0 +1,5 @@
+package org.techtown.request;
+
+public class MovieList {  // 응답 데이터 가장 바깥이 중괄호이므로 이 객체를 반환할 클래스로 MovieList라는 이름의 클래스를 정의한다.
+    MovieListResult movieListResult;  // 변수의 이름은 JSON 문자열에서 속성의 이름과 같아야 한다.
+}
