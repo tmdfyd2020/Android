7d2884bc7ae251a214c672535ce782b459c32494
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-02-21 16:14:28 +0900

    리싸이클러뷰 + 카드뷰 + Volley + JSON 처리

---
+++ Contents/09_서버에 데이터 요청하고 응답받기/06_영화 정보 가져와 보여주기/MovieListResult.java
@@ -0,0 +1,11 @@
+package org.techtown.movie;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
